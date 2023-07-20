3f7f30a1d396ca133b3464fe20a10f5f1a577060
Miguel Acevedo <82989924+macevedoponce@users.noreply.github.com>
2023-07-20 17:29:37 -0500

    generacion de migraciones para bd

---
+++ database/migrations/2023_07_20_213547_campeonatos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('campeonatos', function (Blueprint $table) {
+            $table->engine="InnoDB";
+            $table->bigIncrements('id');
+            $table->string('nombreCampeonato');
+            $table->date('fecha_inicio');
+            $table->date('fecha_fin');
+            $table->bigInteger('tipo_campeonato_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('tipo_campeonato_id')->references('id')->on('tipoCampeonato')->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
